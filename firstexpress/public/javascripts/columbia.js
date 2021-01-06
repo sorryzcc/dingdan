5cb028d6feb1b4c6e3900fff531ff961f7dba912
675623045@qq.com <675623045@qq.com>
2021-01-06 22:46:35 +0800

    增加了搜索流水号功能

--- firstexpress/public/javascripts/columbia.js
+++ firstexpress/public/javascripts/columbia.js
@@ -18,3 +18,27 @@ function myFunction() {
     } 
   }
 }
+
+function myFunction2() {
+  // 声明变量
+  var input2, filter, table, tr, td, i;
+  input2 = document.getElementById("myInput2");
+  filter = input2.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // 循环表格每一行，查找匹配项
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
+
+
